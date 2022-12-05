abf03a87eac7dc5c4d9af5749dcd95c3cf50bd8b
Olamecome <olamecome@ymail.com>
2022-12-05 00:15:21 -0800

    Import function from module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+import hidden_4
 
-    import hidden_4
+if __name__ == "__main__":
 
     for name in dir(hidden_4):
         print(name if name[:2] != "__")
