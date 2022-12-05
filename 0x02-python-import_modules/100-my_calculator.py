78fa4df07e7b88ffe104a521853e4d4fe80d2026
Olamecome <olamecome@ymail.com>
2022-12-05 03:43:20 -0800

    import function from module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,20 +15,15 @@ def main():
 
     if len(sys.argv[1:]) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     if (sys.argv[2] not in ["+", "-", "*", "/"]):
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
 
-    if sys.argv[2] == "+":
-        result = add(int(sys.argv[1]), int(sys.argv[3]))
-    elif sys.argv[2] == "=":
-        result = sub(int(sys.argv[1]), int(sys.argv[3]))
-    elif sys.argv[2] == "*":
-        result = mul(int(sys.argv[1]), int(sys.argv[3]))
-    elif sys.argv[2] == "/":
-        result = div(int(sys.argv[1]), int(sys.argv[3]))
+    oper = {"+" : add, "-" : sub, "*" : mul, "/" : div}
+
+    result = oper[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3]))
 
     print("{} {} {} = {}".
           format(sys.argv[1], sys.argv[2], sys.argv[3], result))
