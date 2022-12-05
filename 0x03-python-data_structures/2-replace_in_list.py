dc4ff046f94dabba3d8ad34f23200abd6b2788c4
Olamecome <olamecome@ymail.com>
2022-12-05 13:14:37 -0800

    importing functions from module

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,11 +3,13 @@
 
 def replace_in_list(my_list, idx, element):
     """
-    A function that replaces an element of 
+    A function that replaces an element of
     a list at a specific position
-    """ 
-    if idx < 0 or idx > len(my_list) - 1:
+    """
+    if idx < 0 or idx >= len(my_list):
         return my_list
+
     if my_list[idx]:
         my_list[idx] = element
+
     return my_list
