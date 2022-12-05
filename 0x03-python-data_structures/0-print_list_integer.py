d475a1df46b91fcf96d1632d5343cfbe710e07ec
Olamecome <olamecome@ymail.com>
2022-12-05 12:52:13 -0800

    importing functions from module

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list=[]):
-    """
-    Function prints items in a list
-    """
-    print("{:d}".format(i) for i in my_list)
+if __name__ == "__print_list_integer__":
+    def print_list_integer(my_list=[]):
+        """
+        Function prints items in a list
+        """
+        print("{:d}".format(i) for i in my_list)
