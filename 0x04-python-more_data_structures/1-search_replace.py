8492c0e75ce4faa612bc827efab79d3aa0a8a51a
Olamecome <olamecome@ymail.com>
2022-12-07 04:48:19 -0800

    More Data structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: x.replace(search, replace), my_list))
+    new_list = list()
+
+    for item in my_list:
+        new_list.append(item) if item != search else new_list.append(replace)
 
     return new_list
